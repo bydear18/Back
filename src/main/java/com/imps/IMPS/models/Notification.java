9edf160e36b3bc562f6b8f4c2ecf63014025bd36
bydear18 <yacademiko@gmail.com>
2024-10-19 17:16:34 +0800

    Update

--- src/main/java/com/imps/IMPS/models/Notification.java
+++ src/main/java/com/imps/IMPS/models/Notification.java
@@ -2,7 +2,6 @@ package com.imps.IMPS.models;
 
 import java.sql.Date;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
