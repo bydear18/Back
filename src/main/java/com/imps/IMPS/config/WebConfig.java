2c618d6bd4dbd5248cb64752335d8f107ff94d66
bydear18 <yacademiko@gmail.com>
2024-10-20 23:01:29 +0800

    change

--- src/main/java/com/imps/IMPS/config/WebConfig.java
+++ src/main/java/com/imps/IMPS/config/WebConfig.java
@@ -1,18 +1,23 @@
-package com.imps.IMPS.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") 
-                .allowedOrigins("https://imps-online.vercel.app") 
+        registry.addMapping("/**")
+                .allowedOrigins("https://imps-online.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*") 
-                .allowCredentials(true); 
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+    
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("https://imps-online.vercel.app");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new FilterRegistrationBean<>(new CorsFilter(source));
     }
 }
